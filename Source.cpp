1932b35e1b6b9b874f2cc974fb16acbe7b3f7f2e
EhMigueh <138417027+EhMigueh@users.noreply.github.com>
2023-07-22 03:20:27 -0300

    actualización
    Moví las funciones, y actualicé sprites de torres.

--- Source.cpp
+++ Source.cpp
@@ -18,7 +18,6 @@
 #define x_arreglo 41
 #define y_arreglo 20
 #define tam_recurso 150
-//#define ESPACIORECURSO 150
 //Valores definidos.
 
 //Inicializaciones de ALLEGRO.
@@ -40,7 +39,6 @@ ALLEGRO_BITMAP* nucleo = NULL;
 ALLEGRO_BITMAP* nucleo_borde = NULL;
 ALLEGRO_BITMAP* torre_arqueros = NULL;
 ALLEGRO_BITMAP* torre_magos = NULL;
-ALLEGRO_BITMAP* torre_canon = NULL;
 ALLEGRO_FONT* fuente_posicion = NULL;
 ALLEGRO_FONT* fuente_piedra = NULL;
 ALLEGRO_FONT* fuente_madera = NULL;
@@ -84,18 +82,213 @@ struct _recurso
 	int cantidad;
 };
 typedef struct _recurso recurso;
-/*
-struct _torre
-{
-	int x;
-	int y;
-	int tipo;
-	int daño;
-	int rango;
-};
-typedef struct _torre torre;
-*/
 //Estructuras.
+
+//Funciones.
+void lector_mapa(char archivo[y_arreglo][x_arreglo]);
+void inicializar_recursos(char archivo[y_arreglo][x_arreglo], recurso bosque[tam_recurso], recurso pedreria[tam_recurso]);
+void colision_bordes();
+void colision_objetos(char archivo[y_arreglo][x_arreglo]);
+void dibujar_objetos(char archivo[y_arreglo][x_arreglo], ALLEGRO_BITMAP* muro, ALLEGRO_BITMAP* camino, ALLEGRO_BITMAP* cespedycamino, ALLEGRO_BITMAP* cespedycamino2, ALLEGRO_BITMAP* cesped, ALLEGRO_BITMAP* arbol, ALLEGRO_BITMAP* roca, ALLEGRO_BITMAP* nucleo, ALLEGRO_BITMAP* nucleo_borde, ALLEGRO_BITMAP* torre_arqueros, ALLEGRO_BITMAP* torre_magos);
+void dibujar_obrero();
+void dibujar_textos(ALLEGRO_FONT* fuente_posicion, ALLEGRO_FONT* fuente_piedra, ALLEGRO_FONT* fuente_madera);
+void construir_torre_arqueros(char archivo[y_arreglo][x_arreglo]);
+void construir_torre_magos(char archivo[y_arreglo][x_arreglo]);
+int buscar_recurso(int x_mapa, int y_mapa, recurso tipo_recurso[tam_recurso]);
+void recolectar_piedra(char archivo[y_arreglo][x_arreglo], recurso pedreria[tam_recurso]);
+void recolectar_madera(char archivo[y_arreglo][x_arreglo], recurso bosque[tam_recurso]);
+void proceso_teclado_input(ALLEGRO_EVENT juego, char archivo[y_arreglo][x_arreglo]);
+void proceso_teclado_output(ALLEGRO_EVENT juego);
+//Funciones.
+
+//Función principal.
+int main()
+{
+	//Declaración variables locacles función principal.
+	char archivo[y_arreglo][x_arreglo];
+	recurso bosque[tam_recurso];
+	recurso pedreria[tam_recurso];
+	//Declaración variables locacles función principal.
+
+	//Inicialización del timer de última recolección.
+	tiempo_ultima_recoleccion = time(NULL);
+	//Inicialización del timer de última recolección.
+
+	//Errores e inicializaciones de ALLEGRO.
+	if (!al_init())
+	{
+		printf("Error al iniciar ALLEGRO");
+		return -1;
+	}
+	ventana = al_create_display(ANCHO, ALTO);
+	al_install_keyboard();
+	eventos = al_create_event_queue();
+	if (!ventana || !al_install_keyboard() || !eventos)
+	{
+		printf("Error al abrir la ventana o instalar teclado o crear cola de eventos");
+		al_destroy_display(ventana);
+		return -1;
+	}
+	temporizador = al_create_timer(1.0 / 60);
+	if (!temporizador)
+	{
+		printf("Error al crear temporizador");
+		al_destroy_event_queue(eventos);
+		al_destroy_display(ventana);
+		return -1;
+	}
+	al_init_image_addon();
+	obrero_derecha = al_load_bitmap("obrero_right.png");
+	obrero_izquierda = al_load_bitmap("obrero_left.png");
+	obrero_arriba = al_load_bitmap("obrero_up.png");
+	obrero_abajo = al_load_bitmap("obrero_down.png");
+	muro = al_load_bitmap("muro.png");
+	camino = al_load_bitmap("camino.png");
+	cespedycamino = al_load_bitmap("cespedycamino.png");
+	cespedycamino2 = al_load_bitmap("cespedycamino2.png");
+	cesped = al_load_bitmap("cesped.png");
+	arbol = al_load_bitmap("arbol.png");
+	roca = al_load_bitmap("roca.png");
+	nucleo = al_load_bitmap("nucleo.png");
+	nucleo_borde = al_load_bitmap("nucleo_borde.png");
+	torre_arqueros = al_load_bitmap("torre_arqueros.png");
+	torre_magos = al_load_bitmap("torre_magos.png");
+	if (!obrero_derecha || !obrero_izquierda || !obrero_arriba || !obrero_abajo || !muro || !camino || !cespedycamino || !cespedycamino2 || !cesped || !arbol || !roca || !nucleo || !nucleo_borde || !torre_arqueros || !torre_magos)
+	{
+		printf("Error al cargar un dibujo de objeto dentro del mapa");
+		al_destroy_timer(temporizador);
+		al_destroy_event_queue(eventos);
+		al_destroy_display(ventana);
+		return -1;
+	}
+	al_init_font_addon();
+	al_init_ttf_addon();
+	fuente_posicion = al_load_font("arial.ttf", 20, 0);
+	fuente_piedra = al_load_font("arial.ttf", 20, 0);
+	fuente_madera = al_load_font("arial.ttf", 20, 0);
+	if (!fuente_posicion || !fuente_piedra || !fuente_madera)
+	{
+		printf("Error al cargar la fuente de la posición o fuente de la piedra o fuente de la madera");
+		al_destroy_timer(temporizador);
+		al_destroy_event_queue(eventos);
+		al_destroy_display(ventana);
+		return -1;
+	}
+	//Errores e inicializaciones de ALLEGRO.
+
+	//Registro de eventos.
+	al_register_event_source(eventos, al_get_display_event_source(ventana));
+	al_register_event_source(eventos, al_get_timer_event_source(temporizador));
+	al_register_event_source(eventos, al_get_keyboard_event_source());
+	al_set_window_title(ventana, "Protect the CORE");
+	al_flip_display();
+	al_start_timer(temporizador);
+	//Registro de eventos.
+
+	//Lector de mapa.
+	lector_mapa(archivo);
+	//Lector de mapa.
+
+	//Inicialización de recursos.
+	inicializar_recursos(archivo, bosque, pedreria);
+	//Inicialización de recursos.
+
+	//Ejecución del juego.
+	while (ejecutandose)
+	{
+		//Evento juego.
+		ALLEGRO_EVENT juego;
+		al_wait_for_event(eventos, &juego);
+		//Evento juego.
+
+		//Cuerpo juego.
+		if (juego.type == ALLEGRO_EVENT_TIMER)
+		{
+			//Llamada función de colisión con bordes.
+			colision_bordes();
+			//Llamada función de colisión con bordes.
+
+			//Limpieza de los dibujos.
+			al_clear_to_color(al_map_rgb(0, 0, 0));
+			//Limpieza de los dibujos.
+
+			//Dibujo objetos.
+			dibujar_objetos(archivo, muro, camino, cespedycamino, cespedycamino2, cesped, arbol, roca, nucleo, nucleo_borde, torre_arqueros, torre_magos);
+			//Dibujo objetos.
+
+			//Dibujo obrero.
+			dibujar_obrero();
+			//Dibujo obrero.
+
+			//Dibujo textos.
+			dibujar_textos(fuente_posicion, fuente_piedra, fuente_madera);
+			//Dibujo textos.
+
+			//Funciones de recolección.
+			recolectar_madera(archivo, bosque);
+			recolectar_piedra(archivo, pedreria);
+			//Funciones de recolección.
+
+			//Actualización dibujos.
+			al_flip_display();
+			//Actualización dibujos.
+
+			//colisión con objetos.
+			colision_objetos(archivo);
+			//colisión con objetos.
+		}
+		//Cuerpo juego.
+
+		//Cerrar juego.
+		else if (juego.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+		{
+			ejecutandose = false;
+		}
+		//Cerrar juego.
+
+		//Llamada función teclado_input.
+		else if (juego.type == ALLEGRO_EVENT_KEY_DOWN)
+		{
+			proceso_teclado_input(juego, archivo);
+		}
+		//Llamada función teclado_input.
+
+		//LLamada función teclado_output.
+		else if (juego.type == ALLEGRO_EVENT_KEY_UP)
+		{
+			proceso_teclado_output(juego);
+		}
+		//LLamada función teclado_output.
+	}
+	//Ejecución del juego.
+
+	//Liberación de memoria.
+	al_destroy_bitmap(obrero_derecha);
+	al_destroy_bitmap(obrero_izquierda);
+	al_destroy_bitmap(obrero_arriba);
+	al_destroy_bitmap(obrero_abajo);
+	al_destroy_bitmap(muro);
+	al_destroy_bitmap(camino);
+	al_destroy_bitmap(cespedycamino);
+	al_destroy_bitmap(cespedycamino2);
+	al_destroy_bitmap(cesped);
+	al_destroy_bitmap(arbol);
+	al_destroy_bitmap(roca);
+	al_destroy_bitmap(nucleo);
+	al_destroy_bitmap(nucleo_borde);
+	al_destroy_bitmap(torre_arqueros);
+	al_destroy_bitmap(torre_magos);
+	al_destroy_timer(temporizador);
+	al_destroy_event_queue(eventos);
+	al_destroy_display(ventana);
+	al_destroy_font(fuente_posicion);
+	al_destroy_font(fuente_piedra);
+	al_destroy_font(fuente_madera);
+	//Liberación de memoria.
+
+	return 0;
+}
+//Función principal.
 
 //Función para leer el mapa.
 void lector_mapa(char archivo[y_arreglo][x_arreglo])
@@ -229,7 +422,7 @@ void colision_objetos(char archivo[y_arreglo][x_arreglo])
 //Colisión con objetos.
 
 //Función para dibujar objetos.
-void dibujar_objetos(char archivo[y_arreglo][x_arreglo], ALLEGRO_BITMAP* muro, ALLEGRO_BITMAP* camino, ALLEGRO_BITMAP* cespedycamino, ALLEGRO_BITMAP* cespedycamino2, ALLEGRO_BITMAP* cesped, ALLEGRO_BITMAP* arbol, ALLEGRO_BITMAP* roca, ALLEGRO_BITMAP* nucleo, ALLEGRO_BITMAP* nucleo_borde, ALLEGRO_BITMAP* torre_arqueros, ALLEGRO_BITMAP* torre_magos, ALLEGRO_BITMAP* torre_canon)
+void dibujar_objetos(char archivo[y_arreglo][x_arreglo], ALLEGRO_BITMAP* muro, ALLEGRO_BITMAP* camino, ALLEGRO_BITMAP* cespedycamino, ALLEGRO_BITMAP* cespedycamino2, ALLEGRO_BITMAP* cesped, ALLEGRO_BITMAP* arbol, ALLEGRO_BITMAP* roca, ALLEGRO_BITMAP* nucleo, ALLEGRO_BITMAP* nucleo_borde, ALLEGRO_BITMAP* torre_arqueros, ALLEGRO_BITMAP* torre_magos)
 {
 	int x_mapa, y_mapa;
 
@@ -294,11 +487,6 @@ void dibujar_objetos(char archivo[y_arreglo][x_arreglo], ALLEGRO_BITMAP* muro, A
 			{
 				al_draw_bitmap(cesped, x_mapa * 40, y_mapa * 40, 0);
 				al_draw_bitmap(torre_magos, x_mapa * 40, y_mapa * 40, 0);
-			}
-			else if (archivo[y_mapa][x_mapa] == 'g')
-			{
-				al_draw_bitmap(cesped, x_mapa * 40, y_mapa * 40, 0);
-				al_draw_bitmap(torre_canon, x_mapa * 40, y_mapa * 40, 0);
 			}
 		}
 	}
@@ -391,23 +579,6 @@ void construir_torre_magos(char archivo[y_arreglo][x_arreglo])
 }
 //Función para construir torre de magos.
 
-//Función para construir torre de cañon.
-void construir_torre_canon(char archivo[y_arreglo][x_arreglo])
-{
-	int x_mapa, y_mapa;
-
-	x_mapa = obrero.x / 40;
-	y_mapa = obrero.y / 40;
-
-	if (archivo[y_mapa][x_mapa] == 'u' && contador_madera >= 25 && contador_piedra >= 25)
-	{
-		archivo[y_mapa][x_mapa] = 'g';
-		contador_madera -= 25;
-		contador_piedra -= 25;
-	}
-}
-//Función para construir torre de cañon.
-
 //Función para buscar recursos.
 int buscar_recurso(int x_mapa, int y_mapa, recurso tipo_recurso[tam_recurso])
 {
@@ -453,7 +624,7 @@ void recolectar_piedra(char archivo[y_arreglo][x_arreglo], recurso pedreria[tam_
 		}
 		else if (archivo[y_mapa][(obrero.x + ANCHOPERSONAJE - 1) / 40] == 'r')
 		{
-			indice = buscar_recurso(x_mapa, y_mapa, pedreria);
+			indice = buscar_recurso((obrero.x + ANCHOPERSONAJE - 1) / 40, y_mapa, pedreria);
 			tiempo_actual = time(NULL);
 			if (tiempo_actual - tiempo_ultima_recoleccion >= 2)
 			{
@@ -468,7 +639,7 @@ void recolectar_piedra(char archivo[y_arreglo][x_arreglo], recurso pedreria[tam_
 		}
 		else if (archivo[(obrero.y + ALTOPERSONAJE - 1) / 40][x_mapa] == 'r')
 		{
-			indice = buscar_recurso(x_mapa, y_mapa, pedreria);
+			indice = buscar_recurso(x_mapa, (obrero.y + ALTOPERSONAJE - 1) / 40, pedreria);
 			tiempo_actual = time(NULL);
 			if (tiempo_actual - tiempo_ultima_recoleccion >= 2)
 			{
@@ -483,7 +654,7 @@ void recolectar_piedra(char archivo[y_arreglo][x_arreglo], recurso pedreria[tam_
 		}
 		else if (archivo[(obrero.y + ALTOPERSONAJE - 1) / 40][(obrero.x + ANCHOPERSONAJE - 1) / 40] == 'r')
 		{
-			indice = buscar_recurso(x_mapa, y_mapa, pedreria);
+			indice = buscar_recurso((obrero.y + ALTOPERSONAJE - 1) / 40, (obrero.x + ANCHOPERSONAJE - 1) / 40, pedreria);
 			tiempo_actual = time(NULL);
 			if (tiempo_actual - tiempo_ultima_recoleccion >= 2)
 			{
@@ -529,7 +700,7 @@ void recolectar_madera(char archivo[y_arreglo][x_arreglo], recurso bosque[tam_re
 		}
 		else if (archivo[y_mapa][(obrero.x + ANCHOPERSONAJE - 1) / 40] == 'a')
 		{
-			indice = buscar_recurso(x_mapa, y_mapa, bosque);
+			indice = buscar_recurso((obrero.x + ANCHOPERSONAJE - 1) / 40, y_mapa, bosque);
 			tiempo_actual = time(NULL);
 			if (tiempo_actual - tiempo_ultima_recoleccion >= 2)
 			{
@@ -544,7 +715,7 @@ void recolectar_madera(char archivo[y_arreglo][x_arreglo], recurso bosque[tam_re
 		}
 		else if (archivo[(obrero.y + ALTOPERSONAJE - 1) / 40][x_mapa] == 'a')
 		{
-			indice = buscar_recurso(x_mapa, y_mapa, bosque);
+			indice = buscar_recurso(x_mapa, (obrero.y + ALTOPERSONAJE - 1) / 40, bosque);
 			tiempo_actual = time(NULL);
 			if (tiempo_actual - tiempo_ultima_recoleccion >= 2)
 			{
@@ -559,7 +730,7 @@ void recolectar_madera(char archivo[y_arreglo][x_arreglo], recurso bosque[tam_re
 		}
 		else if (archivo[(obrero.y + ALTOPERSONAJE - 1) / 40][(obrero.x + ANCHOPERSONAJE - 1) / 40] == 'a')
 		{
-			indice = buscar_recurso(x_mapa, y_mapa, bosque);
+			indice = buscar_recurso((obrero.x + ANCHOPERSONAJE - 1) / 40, (obrero.y + ALTOPERSONAJE - 1) / 40, bosque);
 			tiempo_actual = time(NULL);
 			if (tiempo_actual - tiempo_ultima_recoleccion >= 2)
 			{
@@ -595,8 +766,6 @@ void proceso_teclado_input(ALLEGRO_EVENT juego, char archivo[y_arreglo][x_arregl
 		break;
 	case ALLEGRO_KEY_E: construir_torre_magos(archivo);
 		break;
-	case ALLEGRO_KEY_R: construir_torre_canon(archivo);
-		break;
 	case ALLEGRO_KEY_ESCAPE: ejecutandose = false;
 		break;
 	}
@@ -621,194 +790,3 @@ void proceso_teclado_output(ALLEGRO_EVENT juego)
 	}
 }
 //Función encargada de teclado cuando se deja de pulsar una tecla.
-
-//Función principal.
-int main()
-{
-	//Declaración variables locacles función principal.
-	char archivo[y_arreglo][x_arreglo];
-
-	tiempo_ultima_recoleccion = time(NULL);
-
-	recurso bosque[tam_recurso];
-	recurso pedreria[tam_recurso];
-	recurso tipo_recurso[tam_recurso];
-	//Declaración variables locacles función principal.
-
-	//Errores e inicializaciones de ALLEGRO.
-	if (!al_init())
-	{
-		printf("Error al iniciar ALLEGRO");
-		return -1;
-	}
-	ventana = al_create_display(ANCHO, ALTO);
-	al_install_keyboard();
-	eventos = al_create_event_queue();
-	if (!ventana || !al_install_keyboard() || !eventos)
-	{
-		printf("Error al abrir la ventana o instalar teclado o crear cola de eventos");
-		al_destroy_display(ventana);
-		return -1;
-	}
-	temporizador = al_create_timer(1.0 / 60);
-	if (!temporizador)
-	{
-		printf("Error al crear temporizador");
-		al_destroy_event_queue(eventos);
-		al_destroy_display(ventana);
-		return -1;
-	}
-	al_init_image_addon();
-	obrero_derecha = al_load_bitmap("obrero_right.png");
-	obrero_izquierda = al_load_bitmap("obrero_left.png");
-	obrero_arriba = al_load_bitmap("obrero_up.png");
-	obrero_abajo = al_load_bitmap("obrero_down.png");
-	muro = al_load_bitmap("muro.png");
-	camino = al_load_bitmap("camino.png");
-	cespedycamino = al_load_bitmap("cespedycamino.png");
-	cespedycamino2 = al_load_bitmap("cespedycamino2.png");
-	cesped = al_load_bitmap("cesped.png");
-	arbol = al_load_bitmap("arbol.png");
-	roca = al_load_bitmap("roca.png");
-	nucleo = al_load_bitmap("nucleo.png");
-	nucleo_borde = al_load_bitmap("nucleo_borde.png");
-	torre_arqueros = al_load_bitmap("torre_arqueros.png");
-	torre_magos = al_load_bitmap("torre_magos.png");
-	torre_canon = al_load_bitmap("torre_canon.png");
-	if (!obrero_derecha || !obrero_izquierda || !obrero_arriba || !obrero_abajo || !muro || !camino || !cespedycamino || !cespedycamino2 || !cesped || !arbol || !roca || !nucleo || !nucleo_borde || !torre_arqueros || !torre_magos || !torre_canon)
-	{
-		printf("Error al cargar un dibujo de objeto dentro del mapa");
-		al_destroy_timer(temporizador);
-		al_destroy_event_queue(eventos);
-		al_destroy_display(ventana);
-		return -1;
-	}
-	al_init_font_addon();
-	al_init_ttf_addon();
-	fuente_posicion = al_load_font("arial.ttf", 20, 0);
-	fuente_piedra = al_load_font("arial.ttf", 20, 0);
-	fuente_madera = al_load_font("arial.ttf", 20, 0);
-	if (!fuente_posicion || !fuente_piedra || !fuente_madera)
-	{
-		printf("Error al cargar la fuente de la posición o fuente de la piedra o fuente de la madera");
-		al_destroy_timer(temporizador);
-		al_destroy_event_queue(eventos);
-		al_destroy_display(ventana);
-		return -1;
-	}
-	//Errores e inicializaciones de ALLEGRO.
-
-	//Registro de eventos.
-	al_register_event_source(eventos, al_get_display_event_source(ventana));
-	al_register_event_source(eventos, al_get_timer_event_source(temporizador));
-	al_register_event_source(eventos, al_get_keyboard_event_source());
-	al_set_window_title(ventana, "Protect the CORE");
-	al_flip_display();
-	al_start_timer(temporizador);
-	//Registro de eventos.
-
-	//Lector de mapa.
-	lector_mapa(archivo);
-	//Lector de mapa.
-
-	//Inicialización de recursos.
-	inicializar_recursos(archivo, bosque, pedreria);
-	//Inicialización de recursos.
-
-	//Ejecución del juego.
-	while (ejecutandose)
-	{
-		//Evento juego.
-		ALLEGRO_EVENT juego;
-		al_wait_for_event(eventos, &juego);
-		//Evento juego.
-
-		//Cuerpo juego.
-		if (juego.type == ALLEGRO_EVENT_TIMER)
-		{
-			//Llamada función de colisión con bordes.
-			colision_bordes();
-			//Llamada función de colisión con bordes.
-
-			//Limpieza de los dibujos.
-			al_clear_to_color(al_map_rgb(0, 0, 0));
-			//Limpieza de los dibujos.
-
-			//Dibujo objetos.
-			dibujar_objetos(archivo, muro, camino, cespedycamino, cespedycamino2, cesped, arbol, roca, nucleo, nucleo_borde, torre_arqueros, torre_magos, torre_canon);
-			//Dibujo objetos.
-
-			//Dibujo obrero.
-			dibujar_obrero();
-			//Dibujo obrero.
-
-			//Dibujo textos.
-			dibujar_textos(fuente_posicion, fuente_piedra, fuente_madera);
-			//Dibujo textos.
-
-			//Funciones de recolección.
-			recolectar_madera(archivo, bosque);
-			recolectar_piedra(archivo, pedreria);
-			//Funciones de recolección.
-
-			//Actualización dibujos.
-			al_flip_display();
-			//Actualización dibujos.
-
-			//colisión con objetos.
-			colision_objetos(archivo);
-			//colisión con objetos.
-		}
-		//Cuerpo juego.
-
-		//Cerrar juego.
-		else if (juego.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
-		{
-			ejecutandose = false;
-		}
-		//Cerrar juego.
-
-		//Llamada función teclado_input.
-		else if (juego.type == ALLEGRO_EVENT_KEY_DOWN)
-		{
-			proceso_teclado_input(juego, archivo);
-		}
-		//Llamada función teclado_input.
-
-		//LLamada función teclado_output.
-		else if (juego.type == ALLEGRO_EVENT_KEY_UP)
-		{
-			proceso_teclado_output(juego);
-		}
-		//LLamada función teclado_output.
-	}
-	//Ejecución del juego.
-
-	//Liberación de memoria.
-	al_destroy_bitmap(obrero_derecha);
-	al_destroy_bitmap(obrero_izquierda);
-	al_destroy_bitmap(obrero_arriba);
-	al_destroy_bitmap(obrero_abajo);
-	al_destroy_bitmap(muro);
-	al_destroy_bitmap(camino);
-	al_destroy_bitmap(cespedycamino);
-	al_destroy_bitmap(cespedycamino2);
-	al_destroy_bitmap(cesped);
-	al_destroy_bitmap(arbol);
-	al_destroy_bitmap(roca);
-	al_destroy_bitmap(nucleo);
-	al_destroy_bitmap(nucleo_borde);
-	al_destroy_bitmap(torre_arqueros);
-	al_destroy_bitmap(torre_magos);
-	al_destroy_bitmap(torre_canon);
-	al_destroy_timer(temporizador);
-	al_destroy_event_queue(eventos);
-	al_destroy_display(ventana);
-	al_destroy_font(fuente_posicion);
-	al_destroy_font(fuente_piedra);
-	al_destroy_font(fuente_madera);
-	//Liberación de memoria.
-
-	return 0;
-}
-//Función principal.
